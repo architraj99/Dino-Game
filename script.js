62d21dce057431217b4cb7527965cf016bfc1068
architraj99 <rajarchit99@gmail.com>
2026-05-17 03:19:57 +0530

    Added moving ground and obstacle system
    Added moving ground animation and cactus obstacle generation system.

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let dino = document.getElementById("dino");
+
 let score = document.getElementById("score");
 
+let gameArea = document.querySelector(".game-area");
+
 let isJumping = false;
+
 let position = 60;
+
 let velocity = 0;
+
 let gravity = 0.9;
 
 function jump() {
 
-    if(isJumping) {
+    if (isJumping) {
         return;
     }
 
@@ -21,7 +27,8 @@ function updateJump() {
     velocity -= gravity;
     position += velocity;
 
-    if(position <= 60) {
+    if (position <= 60) {
+
         position = 60;
         velocity = 0;
         isJumping = false;
@@ -29,15 +36,47 @@ function updateJump() {
 
     dino.style.bottom = position + "px";
     requestAnimationFrame(updateJump);
+
 }
+
+function createObstacle() {
+
+    let cactus = document.createElement("div");
+
+    cactus.classList.add("cactus");
+    cactus.innerText = "🌵";
+    gameArea.appendChild(cactus);
+
+    let cactusPosition = -60;
+
+    let obstacleMove =
+        setInterval(function () {
+
+            cactusPosition += 6;
+
+            cactus.style.right =
+                cactusPosition + "px";
+
+            if (cactusPosition > 1000) {
+
+                clearInterval(obstacleMove);
+                cactus.remove();
+            }
+
+        }, 20);
+}
+
+setInterval(function () {
+    createObstacle();
+}, 2200);
 
 document.addEventListener(
     "keydown",
     function (event) {
         if (event.code === "Space") {
+
             jump();
         }
-
     }
 );
 
